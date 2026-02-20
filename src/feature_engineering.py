26663b8f5b607a2ed098ad717fa222d81fc76ff1
Pawar Atharva Nitin <123879019+Atharva0126@users.noreply.github.com>
2026-02-20 21:35:25 +0530

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -4,6 +4,8 @@ def add_features(df):
     df["Year"] = df["Order Date"].dt.year
     df["Month"] = df["Order Date"].dt.month
     df["Month Name"] = df["Order Date"].dt.month_name()
+    df["Year-Month"] = df["Order Date"].dt.to_period("M").astype(str)
+
     df["Profit Ratio"] = df["Profit"] / df["Sales"]
 
     return df
