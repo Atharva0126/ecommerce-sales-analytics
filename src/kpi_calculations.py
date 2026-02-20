551089440cfd35f383e4c10b2cb383ab8d2b260a
Pawar Atharva Nitin <123879019+Atharva0126@users.noreply.github.com>
2026-02-20 21:05:40 +0530

    Update kpi_calculations.py

--- src/kpi_calculations.py
+++ src/kpi_calculations.py
@@ -1,1 +1,15 @@
+def calculate_kpis(df):
 
+    total_sales = df["Sales"].sum()
+    total_profit = df["Profit"].sum()
+    total_orders = df["Order ID"].nunique()
+    avg_order_value = total_sales / total_orders if total_orders else 0
+    profit_margin = (total_profit / total_sales * 100) if total_sales else 0
+
+    return {
+        "Total Sales": total_sales,
+        "Total Profit": total_profit,
+        "Total Orders": total_orders,
+        "Avg Order Value": avg_order_value,
+        "Profit Margin %": profit_margin
+    }
