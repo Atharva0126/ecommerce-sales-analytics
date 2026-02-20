a767b17869ce41d2a9e98f7d5b19980d966fa56b
Pawar Atharva Nitin <123879019+Atharva0126@users.noreply.github.com>
2026-02-20 21:30:14 +0530

    Update data_cleaning.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -3,9 +3,20 @@ import pandas as pd
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
 
+    # Remove duplicates
     df.drop_duplicates(inplace=True)
+
+    # Drop rows where critical values missing
     df.dropna(subset=["Sales", "Profit"], inplace=True)
 
-    df["Order Date"] = pd.to_datetime(df["Order Date"])
+    # --- Robust Date Parsing ---
+    df["Order Date"] = pd.to_datetime(
+        df["Order Date"],
+        errors="coerce",       # converts invalid dates to NaT
+        infer_datetime_format=True
+    )
+
+    # Remove rows where date failed
+    df.dropna(subset=["Order Date"], inplace=True)
 
     return df
