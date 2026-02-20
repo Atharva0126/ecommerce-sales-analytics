fc16f9773b57decb540660d5dec5da73c99fb5e2
Pawar Atharva Nitin <123879019+Atharva0126@users.noreply.github.com>
2026-02-20 21:04:43 +0530

    Update data_cleaning.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+
+    df.drop_duplicates(inplace=True)
+    df.dropna(subset=["Sales", "Profit"], inplace=True)
+
+    df["Order Date"] = pd.to_datetime(df["Order Date"])
+
+    return df
