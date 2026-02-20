329c476137dc8a84b2397ad9546967cf6126e9a6
Pawar Atharva Nitin <123879019+Atharva0126@users.noreply.github.com>
2026-02-20 21:03:08 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,11 @@
+import pandas as pd
+from pathlib import Path
 
+def load_raw_data():
+    path = Path("data/raw/ecommerce_sales.csv")
+    return pd.read_csv(path)
+
+def save_processed_data(df):
+    path = Path("data/processed/cleaned_data.csv")
+    path.parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(path, index=False)
